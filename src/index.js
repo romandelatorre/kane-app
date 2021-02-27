610d7d8bef0d68846819640b25490e66634dbb07
romandelatorre <r.delatorre@ekoomedia.co>
2021-02-27 18:13:40 -0500

    login componentÂ

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Login from './components/Login';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
