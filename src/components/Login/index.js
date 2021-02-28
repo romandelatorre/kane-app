ea1e2752c5b3285bc0f0b2aaeb779727da518966
romandelatorre <r.delatorre@ekoomedia.co>
2021-02-28 16:51:58 -0500

    add logo and styles button

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -11,6 +11,7 @@ import prueba2  from '../.././assets/prueba2.jpg'
 import facebook from '../.././assets/facebook.svg'
 import google from '../.././assets/google.svg'
 import linkedin from '../.././assets/linkedin.svg'
+import logo from '../.././assets/logo.svg'
 
 SwiperCore.use([Navigation, Pagination, Autoplay, EffectFade]);
 
@@ -18,17 +19,21 @@ const App = () => {
 	
 	return (
 		<div className="styles">
+			<img  className="styles__logo" src={logo} alt=""/>
 			<div className="styles__left">
 				<Swiper autoplay={{ delay: 3000 }} loop={true} pagination>
 					<SwiperSlide>
 						<img src={prueba2} alt=""/>
 					</SwiperSlide>
+					
 					<SwiperSlide>
 						<img src={prueba2} alt=""/>
 					</SwiperSlide>
+					
 					<SwiperSlide>
 						<img src={prueba2} alt=""/>
 					</SwiperSlide>
+					
 					<SwiperSlide>
 						<img src={prueba2} alt=""/>
 					</SwiperSlide>
@@ -39,9 +44,9 @@ const App = () => {
 					<h1>Iniciar Sesión</h1>
 					<h4>Selecciona como deseas ingresar a la plataforma</h4>
 					<div className="styles__right__content--buttons">
-						<img src={facebook} alt=""/>
-						<img src={google} alt=""/>
-						<img src={linkedin} alt=""/>
+						<img src={facebook} style={{backgroundColor: "#3A559F"}} alt=""/>
+						<img className="styles__right__content--border" src={google} alt=""/>
+						<img src={linkedin} style={{backgroundColor: '#0077B7'}} alt=""/>
 					</div>
 					<input type="text" placeholder="Correo electronico"/>
 					<input type="password" name="" id=""placeholder="Contraseña"/>
@@ -51,7 +56,7 @@ const App = () => {
 				</div>
 			</div>
 		</div>
-  );
+  	);
 }
 
 export default App;
