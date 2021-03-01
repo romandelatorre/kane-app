35724baf08e23ee803e89b185d08935201ad753f
romandelatorre <r.delatorre@ekoomedia.co>
2021-02-28 19:01:57 -0500

    mixins

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Hola mundo
+        </div>
+    )
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './styles/main.scss';
 import Login from './components/Login';
+import Home from "./components/Home";
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Home/>
+		{/* <Login /> */}
+    </React.StrictMode>,
   document.getElementById('root')
 );
